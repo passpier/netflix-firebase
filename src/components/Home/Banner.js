d737b8dc94716c0f8c238702af1142e008572ed5
passpier <poo9810@gmail.com>
2023-05-03 15:47:53 +0800

    display banner data

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -31,24 +31,23 @@ function Banner() {
       className={styles.banner}
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/220px-Black_flag.svg.png")`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
       }}
     >
       <div className={styles.banner__contents}>
-        <h1 className={styles.banner__title}>Movie Name</h1>
+        <h1 className={styles.banner__title}>
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className={styles.banner__buttons}>
           <button className={styles.banner__button}>Play</button>
           <button className={styles.banner__button}>My List</button>
         </div>
         <h1 className={styles.banner__description}>
-          {truncate(
-            `This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description`,
-            150
-          )}
+          {truncate(movie?.overview, 150)}
         </h1>
       </div>
-      <div className={styles.bannerFadeBottom}></div>
+      <div className={styles.fadeBottom}></div>
     </header>
   );
 }
