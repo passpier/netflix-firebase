8264b0dbc176e3f9fba4258740dd9b7c50d7594f
passpier <poo9810@gmail.com>
2023-05-05 15:52:52 +0800

    add row

---
+++ src/components/Home/Row.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import axios from "../../pages/api/axios";
+import styles from "./Row.module.css";
+
+function Row({ title, fetchUrl, isLargeRow = false }) {
+  const [movies, setMovies] = useState([]);
+
+  const base_url = "https://image.tmdb.org/t/p/original/";
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  return (
+    <div className={styles.row}>
+      <h2>{title}</h2>
+      <div className={styles.row__posters}>
+        {movies.map(
+          (movie) =>
+            ((isLargeRow && movie.poster_path) ||
+              (!isLargeRow && movie.backdrop_path)) && (
+              <img
+                className={`${styles.row__poster} ${
+                  isLargeRow && styles.row__posterLarge
+                }`}
+                key={movie.id}
+                src={`${base_url}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+              />
+            )
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
