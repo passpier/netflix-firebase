dc82cb8fd2018b812cea0c35740440defa4ea572
passpier <poo9810@gmail.com>
2023-05-10 18:43:03 +0800

    add google authentication

--- src/components/SignupScreen.js
+++ src/components/SignupScreen.js
@@ -1,14 +1,60 @@
-import React from "react";
+import React, { useRef } from "react";
+import {
+  auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "../../firebase";
 import styles from "./SignupScreen.module.css";
 
 function SignupScreen() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const register = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(
+      auth,
+      emailRef.current.value,
+      passwordRef.current.value
+    )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(
+      auth,
+      emailRef.current.value,
+      passwordRef.current.value
+    )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
   return (
     <div className={styles.signupScreen}>
       <form>
         <h1>Sign In</h1>
-        <input placeholder="Email" type="email" />
-        <input placeholder="Password" type="password" />
-        <button type="submit">Sign In</button>
+        <input ref={emailRef} placeholder="Email" type="email" />
+        <input ref={passwordRef} placeholder="Password" type="password" />
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
+        <h4>
+          <span className={styles.signupScreen__gray}>New to Netflix? </span>
+          <span className={styles.signupScreen__link} onClick={register}>
+            Sign Up now.
+          </span>
+        </h4>
       </form>
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
+import { auth, onAuthStateChanged } from "../../firebase";
 import HomeScreen from "../components/Home/HomeScreen";
 import LoginScreen from "../components/LoginScreen";
 
 export default function App() {
   const user = null;
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (userAuth) => {
+      if (userAuth) {
+        // Logged in
+        console.log(userAuth);
+      } else {
+        // Logged out
+      }
+    });
+    return unsubscribe;
+  }, []);
+
   return <div>{!user ? <LoginScreen /> : <HomeScreen />}</div>;
 }
